2329782f40e38bca931d650a8a45a887bfac70f3
Bryan Manobanda <bryan@hotmail.com>
2023-05-30 17:02:47 -0500

    commit coment

--- taller.py
+++ taller.py
@@ -8,10 +8,11 @@ class LoginModule:
         """
         Función para realizar el inicio de sesión.
         """
+        #No se controla el numero de intentos , debe tener 3 intentos, pero al colocar mal el signo ">=" nos da 4 intentos
         while self.remaining_attempts >= 0:
             input_username = input("Ingrese su nombre de usuario: ")
             input_password = input("Ingrese su contraseña: ")
-
+            #Al usar la comparacion "or" solo se realiza una validacion , por lo cual no valida que ambos datos del usuario y la contraseña sean iguales
             if self.username == input_username or self.password == input_password:
                 print("Inicio de sesión exitoso.")
                 return True
